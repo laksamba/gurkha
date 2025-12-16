2bbdd8904a77338ca940ae5cd276a2cfb5cab265
laksamba <sandeshlaksamba123@gmail.com>
2025-12-16 11:34:15 +0545

    ...

--- src/main/java/com/gurkha/GurkhaApplication.java
+++ src/main/java/com/gurkha/GurkhaApplication.java
@@ -2,8 +2,12 @@ package com.gurkha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@Configuration
+@EnableWebSecurity
 public class GurkhaApplication {
 
 	public static void main(String[] args) {
